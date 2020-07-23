ee0aa707101ec8ef74dbe45441ff2d965e0107ec
Danish-an <52502341+Danish-an@users.noreply.github.com>
2020-07-23 21:23:03 +0530

    Final Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Interview Page
+It is  a front end in which interviewer can take online interviews of candidates. Interviewer can ask questions and give marks and comments for each question. And after all the questions answered, marks and grades can be view in a result table
+
+# Languages used
+	*HTML, CSS, JavaScript, Bootstrap
+	*Jquery
+	
---
+++ js/custom.js
@@ -0,0 +1,55 @@
+
+var y=0;
+var array = Array();   //declare an array  
+ //pu
+function getComment() {
+
+  
+   var x = document.getElementById("commentbox").value;
+  
+document.getElementById("new").innerHTML = x;
+array[y] = x;
+y++;
+  }
+   function getComment2() {
+
+  
+   x = document.getElementById("commentbox2").value;
+document.getElementById("new2").innerHTML = x;
+array[y] = x;
+y++
+  }
+    function getComment3() {
+
+  
+   x = document.getElementById("commentbox3").value;
+document.getElementById("new3").innerHTML = x;
+array[y] = x;
+y++
+  }
+    function getComment4() {
+
+  
+   x = document.getElementById("commentbox4").value;
+document.getElementById("new4").innerHTML = x;
+array[y] = x;
+y++
+  }
+    function getComment5() {
+
+  
+   x = document.getElementById("commentbox5").value;
+document.getElementById("new5").innerHTML = x;
+array[y] = x;
+y++
+  }
+ function getGrade(){
+  var k = document.getElementById("first").value;
+document.getElementById("grade").innerHTML = k;
+ }
+  function getGrade2(){
+  var k = document.getElementById("first2").value;
+document.getElementById("grade2").innerHTML = k;
+ }
+
+ 
